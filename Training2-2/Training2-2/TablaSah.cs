6e2c68d48d1ad8c71198085252cede38f7927592
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-04 15:11:54 +0300

    Solution for problem " Pătrate pe tabla de șah" -set 2 pb partea2

---
+++ Training2-2/Training2-2/TablaSah.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training2_2
+{
+    [TestClass]
+    public class TablaSah
+    {
+        [TestMethod]
+        public void TestWithATableOf8x8()
+        {
+            Assert.AreEqual(204, CalculateNumberOfSquares(8));
+        }
+
+        [TestMethod]
+        public void TestWithATableOf1x1()
+        {
+            Assert.AreEqual(1, CalculateNumberOfSquares(1));
+        }
+
+
+        private static double CalculateNumberOfSquares(int length)
+        {
+            double sumOfSquares = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sumOfSquares += Math.Pow((double)(length - i), 2);
+            }
+            return sumOfSquares;
+        }
+    }
+}
