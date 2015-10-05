22fb7e4f9f8a0450d4f1eab587d2f5ed4a5d497f
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-05 17:32:37 +0300

    Solution for problem with Anagrams

---
+++ Training2-2/Training2-2/Anagrams.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training2_2
+{
+    [TestClass]
+    public class Anagrams
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string word = "mississippi";
+            Assert.AreEqual(34650, CalculateAnagrams(word));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string word = "math";
+            Assert.AreEqual(24, CalculateAnagrams(word));
+        }
+
+        private static double CalculateAnagrams(string word)
+        {
+            int originalNumberOfChars = word.Length;
+            double product = 1;
+            while(0 < word.Length)
+            {
+                int frequency = 1;
+                for (int j = 1; j < word.Length; j++)
+                {
+                    if (word[0] == word[j])
+                        frequency++;
+                }
+                word = word.Replace(word[0].ToString(),string.Empty);
+                product *= CalculateFactorial(frequency);
+            }
+            double result = CalculateFactorial(originalNumberOfChars) / product;
+            return result;
+        }
+
+        private static double CalculateFactorial(int noToBeFactored)
+        {
+            if (noToBeFactored == 0)
+            {
+                return 1;
+            }
+
+            double multiplication = 1;
+            for (int i = 1; i <= noToBeFactored; i++)
+            {
+                multiplication *= (double)i;
+            }
+
+            return multiplication;
+        }
+    }
+}
